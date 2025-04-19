c9ddb90b85007299f2d30f808bace04fdb67128b
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2025-04-19 03:36:17 +0000

    CREATED INIT FILE TO BOOTSTRAP SQLITE3 DATABASE

--- app.index.js
+++ app.index.js
@@ -1,64 +1,73 @@
 import { FoundryServer } from './generated/server/index.js';
-//import { Database } from './generated/db/index.js';
+import { Database } from './generated/db/init.js';
 
-class NodeService {
-  constructor() {}
-}
+(async function() {
 
-/**
- * @typedef {Object} FoundryOptions
- * @property {MiddlewareOptions} [middleware] - Custom middleware to enhance generated routes
- */
-const options = {
-  NodeService: new NodeService(),
-  middleware: {
-    audit: (req, res, next) => {
-      console.log(`Audting... ${req.path}`);
-      next();
+  class NodeService {
+    constructor() {}
+  }
+
+  /**
+   * @typedef {Object} FoundryOptions
+   * @property {MiddlewareOptions} [middleware] - Custom middleware to enhance generated routes
+   */
+  const options = {
+    NodeService: new NodeService(),
+    middleware: {
+      audit: (req, res, next) => {
+        console.log(`Audting... ${req.path}`);
+        next();
+      },
     },
-  },
-  security: {
-    simpleApiKey: () => (req, res, next) => {
-      const { config, roles } = res.locals.auth;
-      const key = req.headers[config.name];
+    security: {
+      simpleApiKey: () => (req, res, next) => {
+        const { config, roles } = res.locals.auth;
+        const key = req.headers[config.name];
 
-      if (!key) {
-        res.set('content-type', 'application/problem+json');
-        res.status(401);
-        res.send({
-          type: '/probs/authorization-error',
-          title: 'The request could not be authorized',
-          status: 401,
-          detail: 'Access could not be granted to protected resource. Ensure valid authorization credentials are provided in the request. See API specification.',
-          instance: req.path
-        });
-        return;
-      } 
-      // TODO: Create role validation logic
-      /*
-        if (roles.length > 0 && !validateRoles(key, roles)) {
+        if (!key) {
           res.set('content-type', 'application/problem+json');
-          res.status(403);
+          res.status(401);
           res.send({
             type: '/probs/authorization-error',
             title: 'The request could not be authorized',
-            status: 403,
-            detail: 'Access could not be granted to protected resource. The provided authorization credential does not have the required access grants. See administrator.',
+            status: 401,
+            detail: 'Access could not be granted to protected resource. Ensure valid authorization credentials are provided in the request. See API specification.',
+            instance: req.path
           });
           return;
-        }
-      */
-   
-      next();
+        } 
+        // TODO: Create role validation logic
+        /*
+          if (roles.length > 0 && !validateRoles(key, roles)) {
+            res.set('content-type', 'application/problem+json');
+            res.status(403);
+            res.send({
+              type: '/probs/authorization-error',
+              title: 'The request could not be authorized',
+              status: 403,
+              detail: 'Access could not be granted to protected resource. The provided authorization credential does not have the required access grants. See administrator.',
+            });
+            return;
+          }
+        */
+    
+        next();
+      },
     },
-  },
-};
+  };
 
-try {
-  const server = new FoundryServer(options);
-  server.start();
-} catch (ex) {
-  console.error(
-    `INTERNAL_ERROR (App): Could not start server. See details -> ${ex.message}`
-  );
-}
+  try {
+    const statment = Database.prepare('SELECT * FROM clusterinfo');
+    const result = statment.all();
+    console.log({ result });
+
+    const server = new FoundryServer(options);
+    server.start();
+  } catch (ex) {
+    console.error(
+      `INTERNAL_ERROR (App): Could not start server. See details -> ${ex.message}`
+    );
+  }
+}());
+
+
